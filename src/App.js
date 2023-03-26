835e55ad8b9c6b3494c3cc66f340cbcbd7d13f5d
mJaganmohanreddy <jaganre8179@gmail.com>
2023-03-26 16:44:36 +0530

    added captcha sign-up form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import React from 'react';
+
+function SignupForm() {
+  const [name, setName] = React.useState('');
+  const [email, setEmail] = React.useState('');
+  const [password, setpassword] = React.useState('');
+  const [captcha, setcaptcha] = React.useState('');
+
+  function handleSubmit(event){
+    event.preventDefault()
+    console.log({email,password})
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn react
-        </a>
-      </header>
+    <form onSubmit={handleSubmit}>
+      <Field
+        id="name"
+        label="username"
+        value={name}
+        onChange={(event) => {
+          setName(event.target.value);
+        }}
+      />
+      <Field
+        id="email"
+        type="email"
+        label="Email Address"
+        value={email}
+        onChange={(event) => {
+          setEmail(event.target.value);
+        }}
+      />
+       <Field
+        id="password"
+        type="password"
+        label="password"
+        value={password}
+        onChange={(event) => {
+          setpassword(event.target.value);
+        }}
+      />
+      <Field
+          id="password"
+          type='password'
+          label="re-enter password"
+          grow={2}
+          value={password}
+          onChange={(event) => {
+            setpassword(event.target.value);
+          }}
+        />
+      <div className="row">
+      <Field
+          id="captcha"
+          label="Enter-captcha"
+          grow={1}
+          value={captcha}
+          onChange={(event) => {
+            setcaptcha(event.target.value);
+          }}
+        />
+        <div>
+          captcha code
+          <h2  id='captch'style={{backgroundColor:'red', color:'blue'}}>XHhb</h2>
+        </div>
+        
+      </div>
+      <button>Sign up</button>
+    </form>
+  );
+}
+
+function Field({
+  id,
+  label,
+  type = 'text',
+  grow,
+  value,
+  onChange,
+}) {
+  return (
+    <div
+      className="field"
+      style={{ '--grow': grow }}
+    >
+      <label htmlFor={id}>{label}</label>
+      <input
+        type={type}
+        id={id}
+        value={value}
+        onChange={onChange}
+      />
     </div>
   );
 }
 
-export default App;
+export default SignupForm;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,4 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { sendToVercelAnalytics } from './vitals';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-reportWebVitals(sendToVercelAnalytics);
+import { createRoot } from 'react-dom/client';
+import App from './App.js';
+const root = createRoot(document.getElementById('root'));
+root.render(<App />);
